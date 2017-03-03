50420b0b2a2b34610d181f74eba0ca3a2b851a2e
Damien Seguin <dmnsgn@gmail.com>
2017-03-03 16:35:27 +0000

    Handle UI_TOGGLE_MAIN_NAV in ui reducer

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,7 +1,16 @@
-const initialState = {}
+import { UI_TOGGLE_MAIN_NAV } from '../actions'
+
+const initialState = {
+  mainNavOpen: false
+}
 
 const ui = (state = initialState, action) => {
   switch (action.type) {
+    case UI_TOGGLE_MAIN_NAV:
+      return {
+        ...state,
+        mainNavOpen: !state.mainNavOpen
+      }
     default:
       return state
   }
