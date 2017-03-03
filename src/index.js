caee38cfbe5c26923d6d12f5a0cf68f99bc9ecce
Damien Seguin <dmnsgn@gmail.com>
2017-03-03 16:42:42 +0000

    Create the history

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import './index.css'
 
 import React from 'react'
 import { render } from 'react-dom'
+import createHistory from 'history/createBrowserHistory'
 
 import Root from './containers/Root'
 
 import configureStore from './store/configureStore'
 
 const store = configureStore()
+const history = createHistory()
 
 render(
-  <Root store={store} />,
+  <Root store={store} history={history} />,
   document.getElementById('root'),
   () => console.log('Root rendered')
 )
