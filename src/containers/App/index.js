0624ab3a3ed5eeff1b4b23b8404ef846175c5f09
Damien Seguin <dmnsgn@gmail.com>
2017-03-03 14:59:43 +0000

    Create App component + Render App to the DOM

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,9 @@
 import './style.css'
 
-const App = false
+import React from 'react'
+
+const App = ({ title }) => (
+  <h1>{title}</h1>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import './index.css'
 import React from 'react'
 import { render } from 'react-dom'
 
+import App from './containers/App'
+
+const app = <App title="React Redux Workshop" />
+
 render(
-  <h1>React Redux Workshop</h1>,
+  app,
   document.getElementById('root'),
   () => console.log('Root rendered')
 )
