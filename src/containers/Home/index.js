8ce4d0bbab505ec92398272ec8528b3eab36f51f
Damien Seguin <dmnsgn@gmail.com>
2017-03-03 15:25:21 +0000

    Log Home component lifecycle methods

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -6,18 +6,49 @@ class Home extends Component {
   constructor(props) {
     super(props)
 
+    console.log('Home constructor()')
+
     this.state = {
       ready: false
     }
   }
 
+  componentWillMount() {
+    console.log('Home componentWillMount()')
+  }
+
   componentDidMount() {
+    console.log('Home componentDidMount()')
+
     setTimeout(() => {
       this.setState({ ready: true })
     }, 2000)
   }
 
+  componentWillReceiveProps(newProps) {
+    console.log('Home componentWillReceiveProps()')
+  }
+
+  shouldComponentUpdate(newProps, newState) {
+    console.log('Home shouldComponentUpdate()')
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('Home componentWillUpdate()')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Home componentDidUpdate()')
+  }
+
+  componentWillUnmount() {
+    console.log('Home componentWillUnmount()')
+  }
+
   render() {
+    console.log('Home render()')
+
     const { ready } = this.state
 
     if (!ready) return <div>Getting ready...</div>
