fd31d71519b34a180acc003ccdd907809676050f
Damien Seguin <dmnsgn@gmail.com>
2017-03-03 17:40:05 +0000

    Add preloadjs in Scene + Conditionally render background + Load new manifest on url param id change

--- src/containers/Scene/index.js
+++ src/containers/Scene/index.js
@@ -1,66 +1,96 @@
 import './style.css'
 
 import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
+import { translate } from 'react-i18next'
+
+import { default as UtilsPreloader } from '../../utils/preloader'
 
 class Scene extends Component {
   constructor(props) {
     super(props)
-
-    console.log('Scene constructor()')
 
     this.state = {
       ready: false
     }
   }
 
-  componentWillMount() {
-    console.log('Scene componentWillMount()')
-  }
-
   componentDidMount() {
-    console.log('Scene componentDidMount()')
+    const { match } = this.props
+    const sceneId = `scene-${match.params.id}`
 
-    setTimeout(() => {
+    const pAssets = UtilsPreloader.loadManifest(sceneId)
+    pAssets.then(() => {
       this.setState({ ready: true })
-    }, 2000)
+    })
   }
 
-  componentWillReceiveProps(newProps) {
-    console.log('Scene componentWillReceiveProps()')
-  }
+  componentWillReceiveProps(nextProps) {
+    const { match } = this.props
 
-  shouldComponentUpdate(newProps, newState) {
-    console.log('Scene shouldComponentUpdate()')
-    return true;
-  }
+    if (match.params.id !== nextProps.match.params.id) {
+      this.setState({ ready: false })
+      const sceneId = `scene-${nextProps.match.params.id}`
 
-  componentWillUpdate(nextProps, nextState) {
-    console.log('Scene componentWillUpdate()')
+      const pAssets = UtilsPreloader.loadManifest(sceneId)
+      pAssets.then(() => {
+        this.setState({ ready: true })
+      })
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('Scene componentDidUpdate()')
+    const { ready } = this.state
+    const { match } = this.props
+    const sceneId = `scene-${match.params.id}`
+
+    if (!prevState.ready && ready) {
+      createjs.Sound.play(`audio-${sceneId}`, { loop: -1, volume: 0.3 })
+      const content = findDOMNode(this.contentRef)
+      const background = findDOMNode(this.backgroundRef)
+      const title = findDOMNode(this.titleRef)
+
+      content.style.visibility = 'visible'
+      background.style.opacity = 1
+      title.style.opacity = 1
+      title.style.transform = 'translate3d(0, 0, 0)'
+    }
   }
 
   componentWillUnmount() {
-    console.log('Scene componentWillUnmount()')
+    const { match } = this.props
+    const audioId = `audio-scene-${match.params.id}`
+
+    createjs.Sound.stop(audioId)
   }
 
   render() {
-    console.log('Scene render()')
-
     const { ready } = this.state
+    const { match } = this.props
 
-    if (!ready) return <div>Getting ready...</div>
+    const sceneId = `scene-${match.params.id}`
 
-    const divStyle = {
-      visibility: 'visible'
+    let background
+    if (ready) {
+      const backgroundImageItem = UtilsPreloader.getAsset(sceneId, `background-${sceneId}`, true)
+      const backgroundStyle = {
+        backgroundImage: `url(${backgroundImageItem.src})`
+      }
+      background =
+        <div
+          ref={c => this.backgroundRef = c}
+          className="PageContent-background"
+          style={backgroundStyle}
+        />
     }
 
     return (
-      <div className="PageContent Scene" style={divStyle}>Scene Ready</div>
+      <div ref={c => this.contentRef = c} className="PageContent Scene">
+        {background}
+        <h1 ref={c => this.titleRef = c} className="PageContent-title">{sceneId}</h1>
+      </div>
     )
   }
 }
 
-export default Scene
+export default translate([], { wait: true })(Scene)
