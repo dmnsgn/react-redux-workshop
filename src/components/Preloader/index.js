d6d2acccbf0f2bcd67740ba7fde71c67da3aeea6
Damien Seguin <dmnsgn@gmail.com>
2017-03-06 11:45:52 +0000

    Add show, animate and hide on Preloader

--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -1,11 +1,67 @@
 import './style.css'
 
 import React, { Component, PropTypes } from 'react'
+import { findDOMNode } from 'react-dom'
 import { withRouter } from 'react-router'
+import { TweenMax, Sine } from 'gsap'
 
 class Preloader extends Component {
   static propTypes = {
     location: PropTypes.object.isRequired
+  }
+
+  componentDidMount() {
+    this.show()
+  }
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.hide && !this.props.hide) this.hide()
+  }
+
+  show() {
+    const titleGroup = findDOMNode(this.titleGroupRef)
+
+    TweenMax.killTweensOf(titleGroup)
+
+    TweenMax.to(titleGroup, 0.5, {
+      opacity: 1,
+      ease: Sine.easeInOut,
+      onComplete: () => this.animate()
+    })
+  }
+
+  animate() {
+    const titleGroup = findDOMNode(this.titleGroupRef)
+
+    TweenMax.killTweensOf(titleGroup)
+
+    TweenMax.to(titleGroup, 0.5, {
+      scale: 1.2,
+      rotation: -5,
+      ease: Sine.easeInOut,
+      yoyo: true,
+      repeat: 1,
+      onComplete: () => this.props.animateComplete()
+    })
+  }
+
+  hide() {
+    const titleGroup = findDOMNode(this.titleGroupRef)
+    const component = findDOMNode(this)
+
+    TweenMax.killTweensOf(titleGroup)
+    TweenMax.killTweensOf(component)
+
+    TweenMax.to(titleGroup, 0.5, {
+      opacity: 0,
+      ease: Sine.easeInOut,
+    })
+    TweenMax.to(component, 1, {
+      opacity: 0,
+      ease: Sine.easeInOut,
+      delay: 0.5,
+      onComplete: () => this.props.hideComplete()
+    })
   }
 
   render() {
@@ -15,6 +71,7 @@ class Preloader extends Component {
       <div className="Preloader">
         <hgroup
           className="Preloader-titleGroup"
+          ref={c => this.titleGroupRef = c}
         >
           <h1>The Super Title</h1>
           <p>Preloading {location.pathname}</p>
--- src/containers/withPreloader.js
+++ src/containers/withPreloader.js
@@ -4,10 +4,43 @@ import Preloader from '../components/Preloader'
 
 function withPreloader(ComposedComponent) {
   class withPreloaderWrapper extends Component {
+    constructor(props) {
+      super(props)
+
+      this.state = {
+        preloaded: false,
+        destroyed: false
+      }
+    }
+
+    resolveAnimation() {
+      console.log('Preloader animation complete')
+
+      this.setState({
+        preloaded: true
+      })
+    }
+
+    destroyPreloader() {
+      console.log('Preloader destroy')
+
+      this.setState({
+        destroyed: true
+      })
+    }
+
     render() {
+      const { preloaded, destroyed } = this.state
+
       return (
         <main className="Page">
-          <Preloader />
+          {!destroyed &&
+            <Preloader
+              hide={preloaded}
+              animateComplete={this.resolveAnimation.bind(this)}
+              hideComplete={this.destroyPreloader.bind(this)}
+            />
+          }
           <ComposedComponent {...this.props} />
         </main>
       )
