9b6223c9cb0af7fb29f0f244723e09051a2dd926
Damien Seguin <dmnsgn@gmail.com>
2017-03-06 16:49:19 +0000

    Create transition trigger/reset actions and handle them in a `routing` reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ import { createAction } from 'redux-actions';
 
 export const UI_TOGGLE_MAIN_NAV = 'UI_TOGGLE_MAIN_NAV'
 export const toggleMenu = createAction(UI_TOGGLE_MAIN_NAV)
+
+export const ROUTING_TRANSITION_OUT = 'ROUTING_TRANSITION_OUT'
+export const ROUTING_TRANSITION_RESET = 'ROUTING_TRANSITION_RESET'
+export const routingTransitionOut = createAction(ROUTING_TRANSITION_OUT)
+export const routingTransitionReset = createAction(ROUTING_TRANSITION_RESET)
--- src/reducers/routing.js
+++ src/reducers/routing.js
@@ -1,7 +1,21 @@
-const initialState = {}
+import { ROUTING_TRANSITION_OUT, ROUTING_TRANSITION_RESET } from '../actions'
+
+const initialState = {
+  transition: false
+}
 
 const routing = (state = initialState, action) => {
   switch (action.type) {
+    case ROUTING_TRANSITION_OUT:
+      return {
+        ...state,
+        transition: action.payload
+      }
+    case ROUTING_TRANSITION_RESET:
+      return {
+        ...state,
+        transition: false
+      }
     default:
       return state
   }
